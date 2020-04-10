e7d728312dc44a82425f7f82bcd70c0b39d1fdf3
aliselmes <a.selmes@gmail.com>
2020-04-09 21:09:34 -0500

    adds outline code to DataBase Service

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -1,10 +1,37 @@
 namespace recipe_tracker.services 
 {
-    public static class DatabaseService
+    public static class DatabaseService : IDatabaseConnection
     {
-        public static bool SaveRecipe(string name) 
-        {
-            return false;
+        public static Get(string id) {
+            // Code to get data C _R_ U D
+            public IHttpActionResult GetAllRecipes () 
+            {
+
+            }
+        }
+
+        public static Save (object data) {
+            // Code to save data _C_ R U D
+            Post ("http://localhost:5001/recipes/") 
+                {
+                "recipe": {
+                    "ingredients": {
+                        "name": "",
+                        "amount": "",
+                        "measurement": ""
+                    },
+                    "instructions": {
+                        "stepNumber": "",
+                        "instructions": ""
+                    }
+                }
+            }
+            // OR If data already exists, C R _U_ D
+
+        }
+
+        public static Delete (string id) {
+            // Code to Delete data C R U _D_
         }
     }
 }
